464dcdd9b4ed8bbee26917ca100cdc083bfcd2b8
Nivetha Kannan <Nivetha@Nivethas-MacBook-Pro.local>
2015-10-09 17:07:20 -0400

    ds

---
+++ GameStudy/Assets/scripts/Objects/Bucket/Bucket.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+
+class Bucket : MonoBehaviour
+{
+	private bool dragging = false;
+	private float distance;
+	
+	
+	void OnMouseEnter()
+	{
+	
+	}
+	
+	void OnMouseExit()
+	{
+
+	}
+	
+	void OnMouseDown()
+	{
+		distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+		dragging = true;
+		gameObject.layer = 2;
+	}
+	
+	void OnMouseUp()
+	{
+		dragging = false;
+	}
+	
+	void Update()
+	{
+		if (dragging)
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			Vector3 rayPoint = ray.GetPoint(distance);
+			transform.position = rayPoint;
+		}
+	}
+}
---
+++ GameStudy/Assets/scripts/Objects/TentDoor/Tent.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tent : MonoBehaviour {
+
+	public Animator anim;
+	public GameObject tent;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log (anim.GetBool(isOpen), gameObject);
+	
+	}
+
+	void OnMouseDown() {
+		if (gameObject.tag == "closed") {
+			anim.SetBool("isOpen", true);
+			gameObject.tag = "open";
+			
+		} 
+
+		else {
+			anim.SetBool("isOpen", false);
+			gameObject.tag = "closed";
+		}
+
+	}
+}
