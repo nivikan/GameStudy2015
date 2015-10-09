7ff4d6d802881a89e48afcbba5884a3c2fc30d67
Nivetha Kannan <Nivetha@Nivethas-MacBook-Pro.local>
2015-10-09 16:35:45 -0400

    ome assets

---
+++ GameStudy/Assets/Animations/girl2/girl2.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class girl2 : MonoBehaviour {
+
+	public GameObject tent;
+	public Animator anim;
+	
+	// Use this for initialization
+	void Start () {
+		
+		tent = GameObject.Find ("tentOP");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (tent.tag == "open")
+			tentToCamp();
+		
+		
+	}
+	
+	void tentToCamp()
+	{
+		anim.SetInteger("whichScreen", 2);
+	}
+	
+
+}
