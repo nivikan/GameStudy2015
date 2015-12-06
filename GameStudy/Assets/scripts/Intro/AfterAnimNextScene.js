18b27757c25b3f3224fedfb6d4c0482230ab3e31
swethakan <swetha@Swethas-MacBook-Pro.local>
2015-12-06 10:13:22 -0500

    changes

---
+++ GameStudy/Assets/scripts/Intro/AfterAnimNextScene.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+var anim: Animation;
+var Image: GameObject;
+
+function Start() {
+}
+
+
+// Plays an animation only if we are not playing it already.
+function Update() {
+	print(Time.deltaTime >= 0.01660299);
+	if(  Time.deltaTime >= 0.01660299){
+		//Application.LoadLevel("MainGame");
+		}
+}
